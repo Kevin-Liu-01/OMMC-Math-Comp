029286b754e513f6cd06b67d64088070747d0622
ap-1 <67872951+ap-1@users.noreply.github.com>
2020-12-08 19:37:17 -0500

    fix validation

--- name.py
+++ name.py
@@ -1,21 +1,17 @@
-import spacy
-from profanity_filter import ProfanityFilter
+from profanityfilter import ProfanityFilter
 from username_validator import UsernameValidator, PROTOCOL_HOSTNAMES, SENSITIVE_FILENAMES
 
-nlp = spacy.load('en')
 reserved = (PROTOCOL_HOSTNAMES + SENSITIVE_FILENAMES)
-
-profanity_filter = ProfanityFilter(nlps={ "en": nlp })
-nlp.add_pipe(profanity_filter.spacy_component, last=True)
+filter = ProfanityFilter()
 
 class Checker:
     def __init__(self, username_str: str):
         if " " in username_str:
             self.is_valid = False
             return
-        
+
         try:
             UsernameValidator(additional_names=["ommc"], reserved_names=reserved).validate_all(username_str)
-            self.is_valid = nlp(username_str)._.is_profane
+            self.is_valid = filter.is_clean(username_str) and not filter.is_profane(username_str)
         except Exception:
             self.is_valid = False
